190c55450acc6010c9dd98b9d1364e0e0259ea3b
patricksferraz <patrick.ferraz@outlook.com>
2018-10-24 20:05:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/patricksferraz/classcau/view/InitialView.java
@@ -0,0 +1,15 @@
+package br.com.patricksferraz.classcau.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.patricksferraz.classcau.R;
+
+public class InitialView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial_view);
+    }
+}
